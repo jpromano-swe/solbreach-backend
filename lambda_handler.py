9134191ba996c8c5c288a02b5b6448cd70da62a4
Juan <juanpiromanok@gmail.com>
2026-05-27 23:49:17 -0300

    fix: minor adjustments

--- lambda_handler.py
+++ lambda_handler.py
@@ -1,4 +1,5 @@
 from mangum import Mangum
 from app.main import create_app
 
-handler = Mangum(create_app(), lifespan="off")
+app = create_app()
+handler = Mangum(create_app(), lifespan="off", api_gateaway_base_path="/main")
