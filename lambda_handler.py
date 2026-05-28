d9816302b4d5e00c9df566ca0cf40288df5cde35
Juan <juanpiromanok@gmail.com>
2026-05-27 23:58:50 -0300

    fix: minor fix

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,4 +2,5 @@ from mangum import Mangum
 from app.main import create_app
 
 app = create_app()
+stage = os.getenv("STAGE", "")
 handler = Mangum(app, lifespan="off", api_gateway_base_path=f"/{stage}" if stage else None)
